f4f1bf84b56448f505cbc5e396fbba85b088c4a1
Aaron Wright <aaron.c.wright@icloud.com>
2026-04-14 10:39:49 -0500

    Add Apple 1 Hello World

---
+++ hello/hello.asm
@@ -0,0 +1,29 @@
+;-----------------------;
+; Apple I Hello, World! ;
+;-----------------------;
+
+.include "../apple1.inc"
+.segment "CODE"
+ 
+; Start
+Start:
+  lda #$0D            ; CR
+  jsr ECHO            ; Write char to output device
+
+  ldx #0
+Print:
+  lda Message,x
+  beq @PrintEnd
+  jsr ECHO            ; Write char to output device
+  inx
+  jmp Print
+@PrintEnd:
+  lda #$0D            ; CR
+  jsr ECHO            ; Write char to output device
+
+Done:
+  jmp MONRST    ; Jump back to Wozmon
+
+; Message
+Message:
+  .asciiz "HELLO, APPLE I!"
